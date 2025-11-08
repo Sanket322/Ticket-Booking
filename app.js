a1d88c64138b840c09b189e9d5d4ab66189017bd
Sanket Shah <shahsanket322003@gmail.com>
2025-11-08 12:15:30 +0530

    fix: add security packages

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
 const express = require('express');
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+const compression = require("compression");
+const morgan = require("morgan");
 
-
-const user = require("./routes/user");
-const admin = require("./routes/admin");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
 const globalErrorHandler = require("./controller/error_controller");
 
-process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
-
-
 const app = express();
-const port = 8000
-dotenv.config({path : `${__dirname}/config.env`})
-app.use(express.json()) 
 
 
-const DB = process.env.MONGO_URL.replace("<db_password>",process.env.MONGO_PASSWORD)
-mongoose.connect(DB).then(() => {
-  console.log("Database connected successfully")
-})
+app.use(helmet()); // Set Security HTTP Headers
+
+if (process.env.NODE_ENV === "development") {  // Development logging
+  app.use(morgan("dev"));
+}
+
+const limiter = rateLimit({    // Limit requests from same IP
+  max: 100, // 100 requests
+  windowMs: 60 * 60 * 1000, // per hour
+  message: "Too many requests from this IP, try again later!"
+});
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));   // Body parser → reading data from body into req.body
+
+app.use(xss()); // Data sanitization against XSS
+
+app.use(hpp()); // Prevent HTTP Parameter Pollution
+
+app.use(cors()); // Enable CORS
+
+app.use(compression()); // Compress responses
 
 
-app.use("/user", user)
-app.use("/admin", admin)
 
+// Routes
+app.use("/user", userRoutes)
+app.use("/admin", adminRoutes)
+
+
+
+// Invalid Route
 app.all(/.*/, (req, res) => {
   res.statusCode(404).json({
     "error" : "Page Not Found"
   })
 });
 
+
 app.use(globalErrorHandler)
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-
-process.on("unhandledRejection", (err) => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  })
-})
+module.exports = app
